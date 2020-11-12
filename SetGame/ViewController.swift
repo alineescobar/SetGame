c88aa425432b9a215ec51b5d5ed7daacddb218e8
alineescobar <aline.escobar@outlook.com>
2020-11-11 22:15:08 -0300

    task: use the info of the set card to create the button

--- SetGame/ViewController.swift
+++ SetGame/ViewController.swift
@@ -8,15 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+
     var deck = SetCardDeck()
+    
+    private lazy var test = deck.draw()
+//    @IBOutlet private var cardButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        for _ in 1...12 {
+//            if let card = deck.draw() {
+//                print(card.color)
+//                print(card.shading)
+//                print(card.shape)
+//                print(card.number)
+//                print("-------------")
+//            }
+//        }
         
-        for _ in 1...10 {
-            if let card = deck.draw() {
-                print("\(card)")
+    }
+    
+    @IBOutlet weak var testeButton: UIButton! {
+        didSet{
+            switch test?.color {
+            case Optional(SetGame.SetCard.Color.red):
+                testeButton.layer.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+            case Optional(SetGame.SetCard.Color.green):
+                testeButton.layer.backgroundColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1)
+            case Optional(SetGame.SetCard.Color.red):
+                testeButton.layer.backgroundColor = #colorLiteral(red: 0, green: 0.9914394021, blue: 1, alpha: 1)
+            default:
+                testeButton.layer.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
             }
         }
     }
+    
 }
-
